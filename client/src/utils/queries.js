8f777ee263298476e0d3fa2618b7fb55f59238fb
Steven Cruz <cruz801@hotmail.com>
2022-03-18 23:19:36 -0600

    structure

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+// this file stores all the graphql query request
+import { gql } from '@apollo/client';
+
+export const QUERY_THOUGHTS = gql`
+query thoughts($username: String) {
+    thoughts(username: $username) {
+        _id
+        thoughtText
+        createdAt
+        username
+        reactionCount
+        reactions {
+            _id
+            createdAt
+            username
+            reactionBody
+        }
+    }
+}
+`;
